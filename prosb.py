18c1538bc66a20c9602594b42adec7717907c363
ₐᵣᵢfᵢₛₜᵢfᵢₖ <37590868+arifistifik@users.noreply.github.com>
2018-10-20 11:38:57 +0000

    Update prosb.py

--- prosb.py
+++ prosb.py
@@ -1,7 +1,7 @@
 
 # -*- coding: utf-8 -*- 
-import LINEPY
-from LINEPY import *
+import linepy
+from linepy import *
 from akad.ttypes import *
 from multiprocessing import Pool, Process
 from time import sleep
